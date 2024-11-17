a89585947a68d13e2a7b24aad8bf7813a1090157
ander508 <off24001@byui.edu>
2024-11-17 23:14:22 +0100

    Prep 4 Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,66 @@
-using System;
+// using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+        // List<string> fruits = new List<string>();
+        // fruits.Add("Apple");
+        // fruits.Add("Orange");
+        // fruits.Add("Mango");
+        // fruits.Add("Pawpaw");
+        
+        // foreach (string fruit in fruits){
+        //     Console.WriteLine(fruit);
+            
+        // }
+        
+        //     if (fruits.Contains("Apple"))
+        //     {
+        //         Console.WriteLine($"Apple is in the list");
+                
+        //     }
+        //     else{
+        //         Console.WriteLine($"Apple is not in the list");
+                
+        //     }
+           
+        // int length = fruits.Count;
+        // Console.WriteLine(length);
+        // for (int i = 0; i <= fruits.Count-1; i++)
+        // {
+        //     Console.WriteLine(fruits[i]);
+        // }
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int number = 1;
+        List<int> values = new List<int>();
+        
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            
+            if (number != 0)
+            {
+                values.Add(number);
+            }
+        }
+        
+        int sum  = 0;
+        foreach (int value in values){
+            sum += value;
+        }
+        int length = values.Count;
+        double avg = (float)sum /length;
+        int max  = values.Max();
+        Console.WriteLine(string.Join(",", values));
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg:F2}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
