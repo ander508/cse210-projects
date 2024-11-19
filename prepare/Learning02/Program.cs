25e52ad6c19f7a362b2d173070aa6ecff54a77bc
ander508 <off24001@byui.edu>
2024-11-20 00:07:11 +0100

    Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2024;
+        job1._endYear = 2050;
+        Console.WriteLine(job1._company);
+        job1.Display();
+        
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2020;
+        job2._endYear = 2030;
+        Console.WriteLine(job2._company);
+        job2.Display();
+        
+        
+        Resume resume1 = new Resume();
+        resume1._name = "Anderson Offor";
+        resume1._job.Add(job1);
+        resume1._job.Add(job2);
+
+        Console.WriteLine($"{resume1._job[0]._jobTitle}");
+        Console.WriteLine($"{resume1._job[1]._jobTitle}");
+        
+        resume1.Display();
     }
+    
 }
